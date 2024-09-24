72b73b787dabb8208cc4a98c5298ff75784c59b0
Tyler Moroz <moroztyler@gmail.com>
2024-09-24 07:39:37 -0600

    Update functionality.

--- graph.js
+++ graph.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
 
+class SquareNode {
+  constructor(val) {
+    this.val = val;
+    this.neighbors = new Array();
+  }
+}
+
 const isValidMove = (x, y, visited) => {
   return x >= 0 && x < 8 && y >= 0 && y < 8 && !visited[x][y];
 };
 
-const createAdjacencyList = (x, y, visited) => {
+const createAdjacencyList = (x, y) => {
+  const adjList = new Map();
   const directions = [
     [2, 1],
     [1, 2],
@@ -15,28 +23,27 @@ const createAdjacencyList = (x, y, visited) => {
     [-2, 1],
     [-1, 2],
   ];
-
-  const adjacentPositions = [];
+  adjList.set(["x", new Array()]);
+  adjList.set(["y", new Array()]);
 
   for (const [dx, dy] of directions) {
     const nx = x + dx;
     const ny = y + dy;
-    if (isValidMove(nx, ny, visited)) {
-      adjacentPositions.push([nx, ny]);
+    if (!adjList.has(nx)) {
+      adjList.set(nx, new Array());
     }
+    if (!adjList.has(ny)) {
+      adjList.set(ny, new Array());
+    }
+    adjList.get(nx).push(ny);
   }
-  return adjacentPositions;
+  return adjList;
 };
 
-const knightMoves = (start, target) => {
-  const startX = start[0];
-  const startY = start[1];
-  const targetX = target[0];
-  const targetY = target[1];
+// const knightMoves = (start, target) => {
+//   //bfs
+// };
 
-  const visited = Array.from({ length: 8 }, () => Array(8).fill(false));
-};
-
-const adjacencyList = createAdjacencyList(3, 3, visited);
+const adjacencyList = createAdjacencyList(3, 3);
 
 console.log(adjacencyList);
